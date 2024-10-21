d584bc3babacb69d7e1af0538e041a4bfe3a69ef
AlliefJundan <144867147+AlliefJundan@users.noreply.github.com>
2024-10-21 11:12:51 +0700

    Delete kasir directory

--- kasir/pelanggan_hapus.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$id = $_GET['id'];
-$query = mysqli_query($koneksi, "DELETE FROM pelanggan WHERE id_pelanggan =$id");
-if ($query) {
-    echo '<script>alert("Hapus data berhasil"); location.href="?page=pelanggan"</script>';
-} else {
-    echo '<script>alert("Hapus data gagal: ' . mysqli_error($koneksi) . '");</script>';
-}
-?>
